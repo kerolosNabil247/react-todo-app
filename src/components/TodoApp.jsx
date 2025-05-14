ef3d86d37edc7d3e3b3f7209a4e85d1bb1554646
Kerolos Nabil <kerolosnabil247@gmail.com>
2025-05-14 15:30:39 +0300

    handle the data typed by the user in the text box and sent to parent component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import './TodoList.css'
+import React, { useState } from "react";
+import "./TodoList.css";
 
-export default function TodoApp() {
+export default function TodoApp({ onAddClick }) {
+  const [taskData, setTaskData] = useState("");
+
+  const handleClick = () => {
+    if(taskData.trim() !== ''){
+        onAddClick(taskData);
+        setTaskData('');
+    }
+  };
+
   return (
-    <div className='custom-child1-bg m-2 text-white'>
+    <div className="custom-child1-bg m-2 text-white">
       <div className="row m-1 justify-content-center">
-        <h1 className='col-9 text-end'><b>To-Do App!</b></h1>
+        <h1 className="col-9 text-end">
+          <b>To-Do App!</b>
+        </h1>
       </div>
       <div className="row m-1 justify-content-center">
-        <samp className='col-9 text-end'><b>Add New To-Do</b></samp>
+        <samp className="col-9 text-end">
+          <b>Add New To-Do</b>
+        </samp>
       </div>
       <div className="row m-1 justify-content-start">
-        <input type="text" placeholder='Enter New Task' className='col-10 border-0 ms-3' />
+        {/* here handle the input of the user to textbox */}
+        <input
+          type="text"
+          placeholder="Enter New Task"
+          className="col-10 border-0 ms-3"
+          value={taskData}
+          onChange={(e) => {setTaskData(e.target.value)} }
+        />
       </div>
       <div className="row m-1 justify-content-center">
         <div className="col-10 text-end mb-5 mt-3">
-        <button className='btn border border-white text-white'><b>Add</b></button>
+          {/* here handle the add click to send the data to parent component */}
+          <button
+            className="btn border border-white text-white"
+            onClick={handleClick}
+          >
+            <b>Add</b>
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TodoList.css'
 import TodoApp from './TodoApp'
 import Child2 from './Child2'
 
 export default function TodoList() {
+
+    const [tasks, setTasks] = useState([])
+
+    const handleOnAdd = (TodoData) => {
+        // console.log(TodoData);
+        if (TodoData.trim() != ''){ 
+            const newTask = {
+                id: crypto.randomUUID(),
+                text: TodoData,
+                completed: false
+            };
+            setTasks([...tasks, newTask]);
+        }
+    }
   return (
     <div className='container'>
       <div className="parent custom-parent-border">
+        {/* here is the first child */}
         <div className="child1 custom-child-border m-2">
-            <TodoApp></TodoApp>
+            <TodoApp onAddClick={handleOnAdd}></TodoApp>
         </div>
+        {/* here is the second child */}
         <div className="child2 custom-child-border m-2">
             <Child2></Child2>
         </div>
